d75cc2abba7edec444e42abea3c17085cdd32ce0
bhatiasimarjeet <bhatiasimarjeet@gmail.com>
2023-12-24 08:39:46 +0530

    added string_float_integer input file

---
+++ Day-1/string_float_int_input.py
@@ -0,0 +1,12 @@
+
+str_input = input("Enter a String value: ")
+print(type(str_input))
+print(str_input)
+
+int_input = int(input("Enter a integer value: "))
+print(type(int_input))
+print(int_input)
+
+float_input = float(input("Enter a float value: "))
+print(type(float_input))
+print(float_input)
