445e27cb77ac5ba4e2b4b9a51f6c065ecf39eb05
bhatiasimarjeet <bhatiasimarjeet@gmail.com>
2023-12-24 14:39:43 +0530

    programs updated

---
+++ Day-2/area_of_rectangle.py
@@ -0,0 +1,6 @@
+length_of_rectangle = float(input("Enter the length of a rectangle: "))
+breadth_of_rectangle = float(input("Enter the breadth of a rectangle: "))
+
+area_of_rectangle = length_of_rectangle * breadth_of_rectangle
+
+print (area_of_rectangle)
---
+++ Day-2/area_of_square.py
@@ -0,0 +1,5 @@
+side_of_square = float(input("Enter the dimension of side of square: "))
+
+area_of_square = side_of_square * side_of_square
+
+print(area_of_square)
