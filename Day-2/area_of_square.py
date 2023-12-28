c8cf08220f015eba8045cff24f713048271a05e4
bhatiasimarjeet <bhatiasimarjeet@gmail.com>
2023-12-28 15:52:40 +0530

    Added Day-2 all lab programs

--- Day-2/area_of_square.py
+++ Day-2/area_of_square.py
@@ -2,4 +2,4 @@ side_of_square = float(input("Enter the dimension of side of square: "))
 
 area_of_square = side_of_square * side_of_square
 
-print(area_of_square)
+print("Area_of_square is: " + str(area_of_square))
